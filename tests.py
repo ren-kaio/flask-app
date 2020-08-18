e2489df8cb50712fb8f5d9c7d7a3f8ae9015504d
ren.k <ren.kaio@yandex.ru>
2020-08-18 21:02:17 +0300

    tests corrected

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class FlaskAppTests(unittest.TestCase):
 
     def test_get(self):
         r = self.app.get('/')
-        self.assertEqual(r.data, 'I made it!!')
+        self.assertEqual(r.data, b'I made it!!')
 
     def test_post(self):
         r = self.app.post('/')
